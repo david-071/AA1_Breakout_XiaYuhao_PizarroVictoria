6a8cb5a4820482201782f784583fdd6816517b6a
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2025-04-13 02:31:10 +0200

    Update RankingScene.cpp

--- Framework/Framework/RankingScene.cpp
+++ Framework/Framework/RankingScene.cpp
@@ -1,5 +1,8 @@
 #include "RankingScene.h"
+#include <fstream>
+RankingScene::RankingScene() {
 
+}
 void RankingScene::Update()
 {
 	if (GetAsyncKeyState('R') != 0) {
@@ -9,5 +12,11 @@ void RankingScene::Update()
 
 void RankingScene::Render()
 {
+	std::cout << "    ____              __   _            \n";
+	std::cout << "   / __ \____ _____  / /__(_)___  ____ _\n";
+	std::cout << "  / /_/ / __ `/ __ \/ //_/ / __ \/ __ `/\n";
+	std::cout << " / _, _/ /_/ / / / / ,< / / / / / /_/ / \n";
+	std::cout << "/_/ |_|\__,_/_/ /_/_/|_/_/_/ /_/\__, /  \n";
+	std::cout << "                               /____/   \n";
 	std::cout << "Press R to go back to the menu;";
 }
